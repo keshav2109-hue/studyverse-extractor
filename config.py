e37c6c9919bb49ab47c0a31975b2ab3c3c20afbd
keshav2109-hue <kunalkankani5@gmail.com>
2025-09-04 16:31:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,49 +4,49 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "25933223"))
+API_ID = int(os.environ.get("API_ID", "25149550"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","6ef5a426d85b7f01562a41e6416791d3")
+API_HASH = os.environ.get("API_HASH","67b06e7a112cb9b9a5365c5f4fce2495")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8318810657:AAHXwmC9hCw3Yldanp2msE9FZlaInvWlCUE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8308115157:AAE7zwirqDe-zHpdAHEtl2S-FR8OCAznhws")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "ziddiextractorbot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "studyverse_extractor_bot")
 BOT_TEXT = "UG Extractor"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "8458169280"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7899819345"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003014595955"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002388003874"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002982741623")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002735715411")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://ankush23:JYzSGlbjpFezDSWH@cluster0.ulzzd1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003014595955"))
 # -----------------------------------------------
-join = '<a href="https://t.me/+WQ_j-6VK8QwzYjRl">✳️ JOIN BACKUP</a>'
+join = '<a href="https://t.me/StudyVerse_Network_SV">✳️ Join Channel</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "zhonsnowchatbbot" #without @
+ADMIN_BOT_USERNAME = "madhav_izpro" #without @
 
-THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/60W9WcdM/file-358.jpg")
+THUMB_URL = os.environ.get("THUMB_URL", "https://cdna.artstation.com/p/assets/images/images/053/465/272/large/rehan-moazzam-studyverse.jpg?1662288112")
 
 
 
 
 # # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
+# API_ID = int(os.environ.get("API_ID", "25149550"))
+# API_HASH = os.environ.get("API_HASH", "67b06e7a112cb9b9a5365c5f4fce2495")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8308115157:AAE7zwirqDe-zHpdAHEtl2S-FR8OCAznhws")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "studyverse_extractor_bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "7899819345"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7899819345").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002388003874"))
 # MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
+# THUMB_URL = os.environ.get("THUMB_URL", "https://cdna.artstation.com/p/assets/images/images/053/465/272/large/rehan-moazzam-studyverse.jpg?1662288112")
 
